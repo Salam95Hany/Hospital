933f455cdacf3da2f1574fdbcab062a9fa76b2ff
mosafa <jmalm7840@gmail.com>
2025-12-06 19:29:28 +0200

    env

--- Hospital.Angular/src/environments/environment.ts
+++ Hospital.Angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:40950/api/',
-    //apiUrl: 'https://urology-cairo-api.runasp.net/api/',
+    //apiUrl: 'http://localhost:40950/api/',
+    apiUrl: 'https://urology-cairo-api.runasp.net/api/',
 };
